7b0913fef5b33ea7395e9784d5fb76457f5bd790
Travis Struckhoff <sandal2826@gmail.com>
2014-04-18 17:20:43 -0500

    weird

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Waquafied');
+    res.send('Waquafied Now');
 });
 
 var server = app.listen(config.site.port, function() {
