851074de062fe5d88f56d80da5f64abeaa06f58c
Vinit Shah <elysiums.shade@gmail.com>
2014-04-18 16:43:23 -0500

    Simple server which takes http requests and sends a response

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+var express = require('express'),
+    config = require('./config');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Waquafied');
+});
+
+var server = app.listen(config.site.port, function() {
+    console.log('Server listening on port %d', server.address().port)
+});
