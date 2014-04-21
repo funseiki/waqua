65795b0cb3f942f24da5d752f20065166d6a69c8
Vinit Shah <elysiums.shade@gmail.com>
2014-04-21 16:48:14 -0500

    Serve files statically

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express'),
+    path = require('path'),
     config = require('./config');
 
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Waquafied Now');
-});
+app.use(express.static(path.join(__dirname, 'ui/public')));
 
 var server = app.listen(config.site.port, function() {
     console.log('Server listening on port %d', server.address().port)
