d2098a361d0f8a5f84aea0efa089e145a594c6d7
Vinit Shah <elysiums.shade@gmail.com>
2014-04-22 20:42:08 -0500

    Render jade views first, then fall back to static path files

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var express = require('express'),
+    jade = require('jade'),
     path = require('path'),
     config = require('./config');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'ui/public')));
+app.engine('jade', require('jade').__express);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+['about', 'search', 'contaminants', 'action'].forEach(function(route) {
+    app.get('/' + route, function(req, res) {
+        //res.sendfile('ui/public/' + route + '.html')
+        res.render(route);
+    });
+});
+
+app.get('/', function(req, res) {
+    res.render('about');
+});
+
+app.use(express.static(path.join(__dirname, 'ui/public')));
 var server = app.listen(config.site.port, function() {
     console.log('Server listening on port %d', server.address().port)
 });
